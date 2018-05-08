493022e7857bd949f75213f512924aa1c174556e
Axel Olsson <axel.olsson@chasacademy.se>
2018-05-08 14:33:40 +0200

    Add some error handling and fix so form reads location result correctly

--- src/component/Day/Day.js
+++ src/component/Day/Day.js
@@ -1,37 +1,47 @@
-import React, { Component } from 'react';
-import './Day.css';
+import React, { Component } from "react";
+import "./Day.css";
 
 class Day extends Component {
-    constructor(props) {
-        super(props)
-        console.log(this.props.forecastInterval);   
-    }
+  constructor(props) {
+    super(props);
+  }
 
-
-    render () {
-        return(
-            <div className="card">
-                <ul className="card-content">
-                    <p className="card-footer-item">
-                        <li><strong>Date/Time: </strong> {this.props.forecastInterval.dt_txt} </li>
-                        <li><strong> Temperature: </strong>{this.props.forecastInterval.main.temp} &deg; C </li>
-                        <li><strong> Windforce: </strong>{this.props.forecastInterval.wind.speed} &deg; m/s </li>
-                        <li><strong> Humidity: </strong>{this.props.forecastInterval.main.humidity} % </li>
-                        <li>
-                            <img className="" src={`http://openweathermap.org/img/w/${this.props.forecastInterval.weather[0].icon}.png`} title="Title goes here" alt="A weather icon, describing the... weather" />
-                            {this.props.forecastInterval.weather[0].description}
-                        </li>
-                    </p>
-                </ul>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="card">
+        <ul className="card-content">
+          <p className="card-footer-item">
+            <li>
+              <strong>Date/Time: </strong> {this.props.forecastInterval.dt_txt}{" "}
+            </li>
+            <li>
+              <strong> Temperature: </strong>
+              {this.props.forecastInterval.main.temp} &deg; C{" "}
+            </li>
+            <li>
+              <strong> Windforce: </strong>
+              {this.props.forecastInterval.wind.speed} &deg; m/s{" "}
+            </li>
+            <li>
+              <strong> Humidity: </strong>
+              {this.props.forecastInterval.main.humidity} %{" "}
+            </li>
+            <li>
+              <img
+                className=""
+                src={`http://openweathermap.org/img/w/${
+                  this.props.forecastInterval.weather[0].icon
+                }.png`}
+                title="Title goes here"
+                alt="A weather icon, describing the... weather"
+              />
+              {this.props.forecastInterval.weather[0].description}
+            </li>
+          </p>
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default Day;
-
-        
-          
-            
-                
-                
--- src/component/Form/Form.js
+++ src/component/Form/Form.js
@@ -1,146 +1,210 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import Day from '../Day/Day';
-import './Form.css'
+import Day from "../Day/Day";
+import "./Form.css";
 
 class Form extends Component {
   constructor() {
     super();
     this.state = {
       weather: {},
-      latitude:0,
-      longitude:0,
+      latitude: 0,
+      longitude: 0,
       posWeather: {
-          main: {},
-          temp: {},
-          wind: {},
-          weather: [],
-          sys: {},
-          sunrise: {},
-          sunset: {},
-          description: {},
-          id: {}
+        main: {},
+        temp: {},
+        wind: {},
+        weather: [],
+        sys: {},
+        sunrise: {},
+        sunset: {},
+        description: {},
+        id: {}
       },
       forecast: [],
       selectedOption: "Celcius",
       newTemp: "",
       isToggleOn: true
-      
-    }
+    };
+
     this.success = this.success.bind(this);
     this.error = this.error.bind(this);
-    this.handleOptionChange = this.handleOptionChange.bind(this)
+
+    this.handleOptionChange = this.handleOptionChange.bind(this);
   }
 
-  componentDidMount(){
+  componentDidMount() {
     navigator.geolocation.getCurrentPosition(this.success, this.error);
-    }  
-    
+  }
+
   success(pos) {
-    this.setState({
-      longitude: pos.coords.longitude,
-      latitude: pos.coords.latitude
-    })
-    this.getWeather();
+    this.setState(
+      {
+        longitude: pos.coords.longitude,
+        latitude: pos.coords.latitude
+      },
+      function() {
+        this.getWeather();
+      }
+    );
   }
 
   error() {
-    alert('Can not get your current location, you will be placed in stockholm. Dont worry about it!')
-    this.setState({
-      longitude: 18.0685808000000063,
-      latitude: 59.32932349999999
-    })
-    this.getWeather();
+    alert(
+      "Can not get your current location, you will be placed in stockholm. Dont worry about it!"
+    );
+
+    this.setState(
+      {
+        longitude: 18.0685808000000063,
+        latitude: 59.32932349999999
+      },
+      function() {
+        this.getWeather();
+      }
+    );
   }
-  
-  
+
   handleOptionChange() {
     this.setState(prevState => ({
       isToggleOn: !prevState.isToggleOn
     }));
   }
-  
+
   getWeather() {
-    fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${this.state.latitude}&lon=${this.state.longitude}&APPID=5434f1c129e1ac657b10a23c1ac6a1e9&units=metric`)
-    .then(res => res.json())
-    .then(res => {
-      this.setState({
-        posWeather: res
-      }, function() {
-        console.log(res);
-      })
-    });
+    fetch(
+      `http://api.openweathermap.org/data/2.5/weather?lat=${
+        this.state.latitude
+      }&lon=${
+        this.state.longitude
+      }&APPID=5434f1c129e1ac657b10a23c1ac6a1e9&units=metric`
+    )
+      .then(res => res.json())
+      .then(res => {
+        this.setState({
+          posWeather: res
+        });
+      });
   }
-  
+
   //F = 9/5 (K - 273) + 32
   calculateTime(time) {
-    if(time !== undefined) {
+    if (time !== undefined) {
       return new Date(time * 1e3).toISOString().slice(-13, -5);
     }
   }
-  
-  
+
   onSubmit(e) {
     e.preventDefault();
-    
     const cityname = e.nativeEvent.target.elements[0].value;
-    
-    fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${cityname}&APPID=7254d050ae7bb47c60b4718eac4b2132&units=metric`)
-    .then(res => res.json())
-    .then(res => {
-      this.setState({
-        forecast: res.list,
-      }, function() {
-        console.log('Hopefully we have some weather', res);
-      })
-    });
+
+    if (cityname) {
+      fetch(
+        `https://api.openweathermap.org/data/2.5/forecast?q=${cityname}&APPID=7254d050ae7bb47c60b4718eac4b2132&units=metric`
+      )
+        .then(function(response) {
+          if (!response.ok) {
+              alert('Something went wrong, make sure the city name is correct');
+          }
+          return response;
+        })
+        .then(res => res.json())
+        .then(res => {
+          this.setState({
+            posWeather: {
+              name : res.city.name,
+              main: res.list[0].main,
+              wind: res.list[0].wind,
+              weather: res.list[0].weather,
+              sys: {}
+            },
+            forecast: res.list
+          });
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    }
   }
-  
-  
+
   render() {
-    
     return (
       <div>
-
         <form onSubmit={this.onSubmit.bind(this)}>
-          <input type="text" placeholder="Type the city name here" name="city" />
+          <input
+            type="text"
+            placeholder="Type the city name here"
+            name="city"
+          />
           <button type="submit">Get weather</button>
-
         </form>
-          
-        <div className="form-group ">
-            <button onClick={this.handleOptionChange} type="submit">{this.state.isToggleOn ? 'Fahrenheit' : 'Celcius'}</button>
-        </div>
-          {this.state.posWeather ?(
-            <div className="App-weather">
-            <ul>
-            <p>
-            <li><strong>My Location: </strong>{this.state.posWeather.name}</li>
 
-            <li>
-              <strong>Temperature: </strong>{this.state.isToggleOn ? this.state.posWeather.main.temp : ((this.state.posWeather.main.temp * 9 / 5) + 32).toFixed(2)}
-              {this.state.isToggleOn ? <p>&deg;C</p> : <p>&deg;F</p> }
-            </li>
-            <li><strong>Windspeed: </strong>{this.state.posWeather.wind.speed} m/s</li>
-            <li><strong>Humidity: </strong>{this.state.posWeather.main.humidity} %</li>
-            <li><strong>Sunrise: </strong>{this.calculateTime(this.state.posWeather.sys.sunrise)}</li>
-            <li><strong>Sunset: </strong>{this.calculateTime(this.state.posWeather.sys.sunset)}</li>
-            
-            </p>
-            </ul>
-            </div>
-          ):("")}
-        
-        { this.state.forecast && this.state.forecast.length > 0 ?
-        <div>
-          
-          { this.state.forecast.map((forecastInterval, Index) => {
-            return <Day key={Index} forecastInterval={forecastInterval} />
-          })
-        }
+        <div className="form-group ">
+          <button onClick={this.handleOptionChange} type="submit">
+            {this.state.isToggleOn ? "Fahrenheit" : "Celcius"}
+          </button>
         </div>
-        : ''
-      }
+        {this.state.posWeather ? (
+          <div className="App-weather">
+            <ul>
+              <li>
+                <ul>
+                  <li>
+                    <strong>My Location: </strong>
+                    {this.state.posWeather.name
+                      ? this.state.posWeather.name
+                      : "Retreiving your position"}
+                  </li>
+                  <li>
+                    <strong>Temperature: </strong>
+                    {this.state.isToggleOn
+                      ? this.state.posWeather.main.temp
+                      : (this.state.posWeather.main.temp * 9 / 5 + 32).toFixed(
+                          2
+                        )}
+                    {this.state.isToggleOn ? (
+                      <span>&deg;C</span>
+                    ) : (
+                      <span>&deg;F</span>
+                    )}
+                  </li>
+                  <li>
+                    <strong>Windspeed: </strong>
+                    {this.state.posWeather.wind.speed} m/s
+                  </li>
+                  <li>
+                    <strong>Humidity: </strong>
+                    {this.state.posWeather.main.humidity} %
+                  </li>
+                  { this.state.posWeather.sys.sunrise ?
+                  (<li>
+                      <strong>Sunrise: </strong>
+                      {this.calculateTime(this.state.posWeather.sys.sunrise)}
+                  </li>) : ''
+                  }
+                  { this.state.posWeather.sys.sunrise ?
+                  (<li>
+                    <strong>Sunset: </strong>
+                    {this.calculateTime(this.state.posWeather.sys.sunset)}
+                  </li>) : ''
+                  }
+                </ul>
+              </li>
+            </ul>
+          </div>
+        ) : (
+          ""
+        )}
+
+        {this.state.forecast && this.state.forecast.length > 0 ? (
+          <div>
+            {this.state.forecast.map((forecastInterval, Index) => {
+              return <Day key={Index} forecastInterval={forecastInterval} />;
+            })}
+          </div>
+        ) : (
+          ""
+        )}
       </div>
     );
   }
